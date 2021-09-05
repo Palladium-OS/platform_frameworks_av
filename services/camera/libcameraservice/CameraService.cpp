c93d6896013883878b0fc1403959ec3e3b0d5710
chandu078 <chandudyavanapelli03@gmail.com>
2021-09-05 14:25:27 +0000

    [SQUASH]libcameraservice: Drop TARGET_CAMERA_NEEDS_CLIENT_INFO
    Revert "cameraservice: rename aosp to palladium for client_package_name"
    
    This reverts commit 635352ff03304fdeb59c977d68a2e8c8b7e2f621.
    
    Revert "cameraservice: move client_package_name to data/misc"
    
    This reverts commit 3164e95546b3da696765fec9f78fdce8b95a85e3.
    
    Revert "libcameraservice: add TARGET_CAMERA_NEEDS_CLIENT_INFO"
    
    This reverts commit f13f57bfbf8d32b01f9d352a47e719a00f49ab71.
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -24,10 +24,6 @@
 #include <cstring>
 #include <ctime>
 #include <string>
-#ifdef TARGET_NEEDS_CLIENT_INFO
-#include <iostream>
-#include <fstream>
-#endif
 #include <sys/types.h>
 #include <inttypes.h>
 #include <pthread.h>
@@ -3012,12 +3008,6 @@ status_t CameraService::BasicClient::startCameraOps() {
 
     // Notify listeners of camera open/close status
     sCameraService->updateOpenCloseStatus(mCameraIdStr, true/*open*/, mClientPackageName);
-
-#ifdef TARGET_NEEDS_CLIENT_INFO
-	std::ofstream cpf("/data/misc/palladium/client_package_name");
-	std::string cpn = String8(mClientPackageName).string();
-    cpf << cpn;
-#endif
 
     return OK;
 }
